13cce0b5862c78c208f8e308fee349471ae148ee
gabbysandrade <gabbysandrade@gmail.com>
2023-01-24 12:45:09 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+/*******************
+Name: Gabriela Andrade
+EBQ Demo
+Purpose: This script will cause contact icons to appear
+when the "Contact Us" button on the bottom of the EBQ
+landing page is clicked.
+********************/
